be62afa06c7183894b31de067b0263a8d7b440c4
VeekyE <vicoluchukwu18@gmail.com>
2023-02-20 18:32:19 -0800

    Task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,34 +8,42 @@
 
 int main(void)
 {
+	int n;
+	int z;
+	int y;
+	int p;
+	int p1;
+	int p2;
 
-        int n, z, y, p;
+	for (p = 48; p < 58; p++)
+	{
+		for (y = 48; y < 57; y++)
+		{
+			for (z = 48; z < 58; z++)
+			{
+				for (n = 48; n < 58; n++)
+				{
+					p1 = (p * 10) + y;
+					p2 = (z * 10) + n;
+					if (p1 < p2)
+					{
+						putchar(p);
+						putchar(y);
+						putchar(' ');
+						putchar(z);
+						putchar(n);
+						if (p == 57 && y == 56 && z == 57 && n == 57)
+						{
+							break;
+						}
+						putchar(',');
+						putchar(' ');
 
-        for (n = 48; n <= 57 ; n++)
-        {
-                for (z =  48; z <= 56; z++)
-                {
-                    
-                    for (y =  48 ; y <= 57; y++)
-                    {
-                        for (p = 48 ; p <= 57; p++)
-                        {
-                            putchar(n);
-                            putchar(z);
-                            putchar(' ');
-                            putchar(y);
-                            putchar(p);
-                              if (n != 57 || z != 56 || y != 57 || p != 57)
-                              {
-                                  putchar(' ');
-                                  putchar(',');
-                              }
-              
-                        }
-          
-                    }
-
-                }
-        }
-        return (0);
+					}
+				}
+			}
+		}
+	}
+	return (0);
 }
+

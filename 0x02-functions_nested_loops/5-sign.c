bf32f7f5a67f1b877bf0c716c3b9b6739c90f12c
VeekyE <vicoluchukwu18@gmail.com>
2023-02-22 01:33:42 -0800

    Task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,32 +2,26 @@
 /**
  * print_sign - function that prints sign
  *
- * @c: function parameter
+ * @n: function parameter
  *
  * Return: 1, 0 and -1
  */
+
 int print_sign(int n)
 {
-
 	if (n > 0)
 	{
 		_putchar('+');
-		_putchar(',');
-		_putchar(' ');
 		return (1);
 	}
-	else if (n == 0)
+	else if (n < 0)
 	{
-		_putchar(0);
-		_putchar(',');
-		_putchar(' ');
-		return (0);
+		_putchar('-');
+		return (-1);
 	}
 	else
 	{
-		_putchar('-');
-		_putchar(',');
-		_putchar(' ');
-		return (-1);
+		_putchar('0');
+		return (0);
 	}
 }

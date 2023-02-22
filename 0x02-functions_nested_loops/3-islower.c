d38f944dd4dccb394fa91a7bf780d4b8aba7f49e
VeekyE <vicoluchukwu18@gmail.com>
2023-02-21 16:30:37 -0800

    Task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,8 +3,7 @@
 /**
  * _islower - function to print lower case
  * 0c: parameter to be printed
- * Return: 1 if it is a lower case
- * and 0 otherwise
+ * Return: 1 if it is a lower case and 0 otherwise
  */
 
 int _islower(int c)

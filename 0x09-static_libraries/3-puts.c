f978f859f4a4c3fa284bfde4b42d715c347a1fc7
VeekyE <vicoluchukwu18@gmail.com>
2023-03-10 16:07:32 -0800

    Task 0

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _puts - _puts function
+ * @str: string
+ *
+ * Return: Always 0 (Success)
+ */
+void _puts(char *str)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
